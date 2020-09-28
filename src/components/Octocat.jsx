d4c6a5ba991f18d0da6b76627c8201bb6c92d7a9
Josh Mann <jlmann72@gmail.com>
2020-09-28 17:22:44 -0400

    removed 'this' references from Octocat function

--- src/components/Octocat.jsx
+++ src/components/Octocat.jsx
@@ -3,28 +3,28 @@ import React from 'react'
 export function Octocat(props) {
   return (
     <article>
-      <a href={this.props.bigImageLink}>
+      <a href={props.bigImageLink}>
         <img
-          src={this.props.bigImage}
+          src={props.bigImage}
           width="400"
           height="400"
-          alt={this.props.bigImageAlt}
+          alt={props.bigImageAlt}
         />
       </a>
       <ul>
         <li>
-          {this.props.octodexNumber}
-          <a href={this.props.bigImageLink}>
-            <strong>{this.props.bigImageAlt}</strong>
+          {props.octodexNumber}
+          <a href={props.bigImageLink}>
+            <strong>{props.bigImageAlt}</strong>
           </a>
         </li>
         <li>
-          <a href={this.props.authorOneLink}>
+          <a href={props.authorOneLink}>
             <img
-              src={this.props.authorOneSrc}
+              src={props.authorOneSrc}
               width="24px"
               height="24px"
-              alt={this.props.authorOneAlt}
+              alt={props.authorOneAlt}
             />
           </a>
         </li>
