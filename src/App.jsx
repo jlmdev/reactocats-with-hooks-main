e889d8d0a0db2f46812eb4602cecc14fd1433960
Josh Mann <jlmann72@gmail.com>
2020-09-28 17:28:42 -0400

    App and Octocat functions defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react'
+import { PageHeader } from './components/PageHeader'
+import { Octocat } from './components/Octocat'
+import Octocats from './components/Octocats.json'
 
 function App() {
-  return <div>Hello, World!</div>
+  const octocatsFromData = Octocats.map((octocat) => (
+    <Octocat
+      key={octocat.id}
+      bigImageLink={octocat.bigImageLink}
+      bigImage={octocat.bigImage}
+      bigImageAlt={octocat.bigImageAlt}
+      octodexNumber={octocat.octodexNumber}
+      authorOneLink={octocat.authorOneLink}
+      authorOneSrc={octocat.authorOneSrc}
+      authorOneAlt={octocat.authorOneAlt}
+    />
+  ))
+
+  return (
+    <div>
+      <PageHeader />
+      <div className="spacer"></div>
+      <body>
+        <main>
+          <section>{octocatsFromData}</section>
+        </main>
+        <footer>
+          <div>
+            <p>© 2013 – 2020 GitHub, Inc. All rights reserved.</p>
+          </div>
+        </footer>
+      </body>
+    </div>
+  )
 }
 
 export default App
